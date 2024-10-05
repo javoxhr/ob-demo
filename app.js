1ba9416bceb181c2379ce439cc79c89384bea230
javoxhr <javohiruzum@gmail.com>
2024-10-06 00:37:29 +0500

    new

--- app.js
+++ app.js
@@ -122,8 +122,14 @@ fetch("https://raw.githubusercontent.com/javoxhr/data/main/data.json")
                         const usersInfoWrapper = document.querySelector('.users-profil-info')
                         const usersInfoOverlay = document.querySelector('.users-profil-overlay')
                         const usersInfoModal = document.querySelector('.users-profil')
+                        const backBtn = document.querySelector('.back-to-home-btn')
 
                         usersInfoOverlay.addEventListener('click', ()=> {
+                            usersInfoOverlay.style.display = "none"
+                            usersInfoModal.style.display = "none"
+                        })
+
+                        backBtn.addEventListener('click', ()=> {
                             usersInfoOverlay.style.display = "none"
                             usersInfoModal.style.display = "none"
                         })
